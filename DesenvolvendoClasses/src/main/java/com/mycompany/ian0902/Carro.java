2174ac6ca870500b9874843d98893afcdf97cd96
IanlBello <166515946+IanBd00@users.noreply.github.com>
2026-02-09 20:36:33 -0300

    Add files via upload

---
+++ DesenvolvendoClasses/src/main/java/com/mycompany/ian0902/Carro.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ian0902;
+
+/**
+ *
+ * @author alunolab13
+ */
+public class Carro {
+    String modelo;
+    int ano;
+    
+    String imprimir(){
+    return "Modelo: " + modelo + System.lineSeparator() + "Ano: " + ano;
+}
+}
---
+++ DesenvolvendoClasses/src/main/java/com/mycompany/ian0902/Principal.java
@@ -0,0 +1,21 @@
+                    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ian0902;
+
+/**
+ *
+ * @author alunolab13
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        Carro c1 = new Carro();
+        c1.modelo = "HB20";
+        c1.ano = 2022;
+        
+        System.out.println(c1.imprimir());
+              
+    }
+}
