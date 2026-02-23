6fa2c0fbbfed40fc8b4bdd06db2b0f049ed1af7b
IanlBello <166515946+IanBd00@users.noreply.github.com>
2026-02-23 20:43:08 -0300

    Add files via upload

--- CreditoeDebito/src/main/java/com/mycompany/creditoedebito/Conta.java
+++ CreditoeDebito/src/main/java/com/mycompany/creditoedebito/Conta.java
@@ -9,17 +9,69 @@ package com.mycompany.creditoedebito;
  * @author alunolab13
  */
 public class Conta {
-    private double saldo;
+    private double saldo, limite;
+    private int agencia, numero;
     
-    public double extrato(){
+    public Conta(int agencia, int numero, double saldo, double limite){
+        this.agencia = agencia;
+        this.numero = numero;
+        this.limite = limite;
+        this.saldo = saldo;        
+        
+    }
+    
+    public Conta(double saldoInicial){
+        this.saldo = saldoInicial;
+    }
+    
+    public void transferir(Conta destino, double valor){
+        debitar(valor);
+        destino.creditar(valor);
+    }
+    
+    public double getSaldo(){
         return saldo;
     }
     
+    public void setAgencia(int agencia){
+        this.agencia = agencia;
+    } 
+    
+    public int getAgencia(){
+        return agencia;
+    }
+    
+    
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+    
+    public int getNumero(){
+        return numero;
+    }
+    
+    public double setLimite(double limite){
+        this.limite = limite;
+    }
+    
+    public double getLimite(){
+        return limite;
+    }
+    
+    public Conta(){
+        
+    }
+    
+    
     public void creditar(double valor){
-        saldo = saldo + valor;
+        saldo += valor;
     }
     
     public void debitar(double valor){
-        saldo = saldo - valor;
+        if(valor > saldo + limite){
+        System.out.prinln("ERRO: Seu saldo é menor que o valor de débito");
+        } else{
+        saldo -= valor;
+    }
     }
 }
--- CreditoeDebito/src/main/java/com/mycompany/creditoedebito/Principal.java
+++ CreditoeDebito/src/main/java/com/mycompany/creditoedebito/Principal.java
@@ -11,11 +11,17 @@ package com.mycompany.creditoedebito;
 public class Principal {
 
     public static void main(String[] args) {
-        Conta c1 = new Conta();
+        Conta c1 = new Conta(123,6630,5000);
+        Conta c2 = new Conta();
+        c2.setAgencia(124);
+        c2.setNumero(550);
+        c2.creditar(15000);
+
+        c1.transferir(c2, 4000);
         
-        c1.creditar(450);
-        c1.debitar(100);
-        System.out.println("Saldo: R$" + c1.extrato());
+        
+        System.out.println("Saldo c1: R$" + c1.getSaldo());
+        System.out.println("Saldo c2: R$" + c2.getSaldo());
         
         
         
